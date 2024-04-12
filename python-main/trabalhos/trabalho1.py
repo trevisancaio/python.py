34c775fdd10aaa19b7c4567c1e9f1fcc14b17974
trevisancaio <133895557+trevisancaio@users.noreply.github.com>
2024-04-12 11:39:03 -0300

    Update trabalho1.py

--- python-main/trabalhos/trabalho1.py
+++ python-main/trabalhos/trabalho1.py
@@ -1,27 +1,30 @@
-peso = float(input("Digite um peso:"))
-altura= float(input("Digite uma altura:"))
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2)
 
-calculadora_de_imc = peso/(altura*altura)
-print( "O imc é:",calculadora_de_imc)
+while True:
+    altura = float(input("Digite sua altura em metros: "))
+    if altura <= 0:
+        break
+        
 
-if peso<=0:
-    print("Digite um peso valido")
-    
-elif altura<=0:
-    print("Digite uma altura valida")
- 
-elif calculadora_de_imc < 18.5: 
-    print("Abaixo do peso")
-elif calculadora_de_imc > 18.5 and calculadora_de_imc < 24.9:
-    print("peso normal")
-elif calculadora_de_imc > 25.0 and calculadora_de_imc < 29.9:
-    print("Sobrepeso")
-elif calculadora_de_imc > 30.0 and calculadora_de_imc < 34.9:
-    w = calculadora_de_imc * 0.2
-    print("Obesidade grau 1, precisa perder:",w)
-elif calculadora_de_imc > 35.0 and calculadora_de_imc < 39.9:
-    p = calculadora_de_imc * 0.2
-    print("Obesidade grau 2 (severa), precisa perder:",p)
-else:
-    o = calculadora_de_imc *
-    print("Obesidade grau 3 (mórbida),precisa perder:",o)
+    peso = float(input("Digite seu peso em quilogramas: "))
+    if peso <= 0:
+        break
+     
+
+    imc = calcular_imc(peso, altura)
+    imc_arredondado = round(imc, 2)
+    print(f"Seu IMC é: {imc_arredondado}")
+
+    if imc_arredondado <= 18.4:
+        print("Você está abaixo do peso.")
+    elif imc_arredondado <= 24.9:
+        print("Seu peso está normal.")
+    elif imc_arredondado <= 29.9:
+        print("Você está acima do peso.")
+    elif imc_arredondado <= 34.9:
+        print("Você está com obesidade grau I.")
+    elif imc_arredondado <= 39.9:
+        print("Você está com obesidade grau II.")
+    else:
+        print("Você está com obesidade grau III.")
